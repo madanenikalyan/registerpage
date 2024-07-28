5e916942d523a8a3ec65a4c0c3915c68fc17b140
madanenikalyan <112858372+madanenikalyan@users.noreply.github.com>
2024-07-28 17:56:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('register-btn').addEventListener('click', function() {
+  document.querySelector('.login-container').style.display = 'none';
+  document.querySelector('.register-container').style.display = 'block';
+});
